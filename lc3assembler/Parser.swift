d047b4abfa54b36073a39dc1f2416df7942b2cc4
Mert Dümenci <mert@dumenci.me>
2017-02-13 23:09:08 -0500

    Preliminary work in parsing assembler directives

--- lc3assembler/Parser.swift
+++ lc3assembler/Parser.swift
@@ -8,33 +8,148 @@
 
 import Foundation
 
+let space: Character = " "
+
+func tokenize(line: String) -> [String] {
+    return line.characters.split(separator: space).map() { sequence in
+        return String(sequence).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
+
 struct ConditionCode: OptionSet {
     let rawValue: Int
     
-    static let negative = (1 << 0)
+    static let negative = (1 << 2)
     static let zero = (1 << 1)
-    static let positive = (1 << 2)
+    static let positive = (1 << 0)
+}
+
+struct Resolvable<T> {
+    let value: T?
+    let symbol: String?
+    
+    func resolve(table: SymbolTable?) -> T? { // TODO: throws unresolvable error
+        if let value = value {
+            return value
+        }
+        
+        // TODO: resolve symbol
+        return nil
+    }
+}
+
+protocol BinaryRepresentable {
+    func data() -> NSData
+}
+
+enum AssemblerDirective {
+    case COMMENT(comment: String)
+    case ORIG(address: UInt16)
+    case FILL(data: UInt16)
+    case BLKW(length: UInt)
+    case STRINGZ(string: String)
+    case END
+    
+    var opcode: String {
+        switch self {
+        case .COMMENT:
+            return ";"
+        case .ORIG:
+            return ".ORIG"
+        case .FILL:
+            return ".FILL"
+        case .BLKW:
+            return ".BLKW"
+        case .STRINGZ:
+            return ".STRINGZ"
+        case .END:
+            return ".END"
+        }
+    }
+    
+    static var opcodes: [String] {
+        // TODO: I hate this -- find a better (?) way of initializing all cases
+        let allDirectives: [AssemblerDirective] =
+            [.COMMENT(comment: String()), .ORIG(address: 0), .FILL(data: 0), .BLKW(length: 0), .STRINGZ(string: String()), .END]
+        return allDirectives.map() { $0.opcode }
+    }
+}
+
+extension AssemblerDirective: BinaryRepresentable {
+    func data() -> NSData {
+        return NSData() // TODO: Implement
+    }
+}
+
+extension AssemblerDirective {
+    func parse(line: String) -> AssemblerDirective? {
+        let tokens = tokenize(line: line)
+        
+        let opcode: String
+        let operands: ArraySlice<String>
+        
+        if let first = tokens.first, AssemblerDirective.opcodes.contains(first) {
+            opcode = first
+            operands = tokens[1..<tokens.count]
+        } else if tokens.count > 1, AssemblerDirective.opcodes.contains(tokens[1]) {
+            opcode = tokens[1]
+            operands = tokens[2..<tokens.count]
+        } else {
+            return nil
+        }
+        
+        switch opcode {
+        case ";":
+            let comment = operands.joined(separator: String(space))
+            return .COMMENT(comment: comment)
+        case ".ORIG":
+            guard operands.count == 1, let address = UInt16(operands[0]) else {
+                return nil
+            }
+            
+            return .ORIG(address: address)
+        case ".FILL":
+            guard operands.count == 1, let data = UInt16(operands[0]) else { // TODO: Implement proper integer parsing (#x)
+                return nil
+            }
+            
+            return .FILL(data: data)
+        case ".BLKW":
+            guard operands.count == 1, let length = UInt(operands[0]) else {
+                return nil
+            }
+        
+            return .BLKW(length: length)
+        case ".STRINGZ":
+            let string = operands.joined(separator: String(space)) // TODO: Implement parsing string in quotes
+            return .STRINGZ(string: string)
+        case ".END":
+            return .END
+        default:
+            return nil
+        }
+    }
 }
 
 enum Instruction {
     case ADD(dataRegister: UInt8, sourceRegister1: UInt8, sourceRegister2: UInt8)
-    case ADD_IMMEDIATE(dataRegister: UInt8, sourceRegister: UInt8, immediateValue: UInt8)
+    case ADD_IMMEDIATE(dataRegister: UInt8, sourceRegister: UInt8, immediateValue: Int8)
     case AND(dataRegister: UInt8, sourceRegister1: UInt8, sourceRegister2: UInt8)
-    case AND_IMMEDIATE(dataRegister: UInt8, sourceRegister: UInt8, immediateValue: UInt8)
-    case BR(conditionCode: ConditionCode, offset: UInt8)
+    case AND_IMMEDIATE(dataRegister: UInt8, sourceRegister: UInt8, immediateValue: Int8)
+    case BR(conditionCode: ConditionCode, offset: Resolvable<UInt16>)
     case JMP(baseRegister: UInt8)
-    case JSR(offset: UInt16)
+    case JSR(offset: Resolvable<UInt16>)
     case JSRR(baseRegister: UInt8)
-    case LD(dataRegister: UInt8, offset: UInt8)
-    case LDI(dataRegister: UInt8, offset: UInt8)
-    case LDR(dataRegister: UInt8, baseRegister: UInt8, offset: UInt8)
-    case LEA(dataRegister: UInt8, offset: UInt8)
+    case LD(dataRegister: UInt8, offset: Resolvable<UInt16>)
+    case LDI(dataRegister: UInt8, offset: Resolvable<UInt16>)
+    case LDR(dataRegister: UInt8, baseRegister: UInt8, offset: Resolvable<UInt16>)
+    case LEA(dataRegister: UInt8, offset: Resolvable<UInt16>)
     case NOT(dataRegister: UInt8, sourceRegister: UInt8)
     case RET
     case RTI
-    case ST(sourceRegister: UInt8, offset: UInt8)
-    case STI(sourceRegister: UInt8, offset: UInt8)
-    case STR(sourceRegister: UInt8, baseRegister: UInt8, offset: UInt8)
+    case ST(sourceRegister: UInt8, offset: Resolvable<UInt16>)
+    case STI(sourceRegister: UInt8, offset: Resolvable<UInt16>)
+    case STR(sourceRegister: UInt8, baseRegister: UInt8, offset: Resolvable<UInt16>)
     case TRAP(trapVector: UInt8)
     
     var opCode: UInt8 {
@@ -78,5 +193,11 @@ enum Instruction {
         case .TRAP:
             return 0xF
         }
+    }
+}
+
+extension Instruction: BinaryRepresentable {
+    func data() -> NSData {
+        return NSData() // TODO: Implement
     }
 }
